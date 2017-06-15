b51b8358f61d5c95fad31ee421827bee84cec472
Tanya <tochka0201@gmail.com>
2017-06-15 12:16:59 +0300

    Register design

---
+++ app/src/main/java/com/example/emily/triptalk/LoginActivity/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.example.emily.triptalk.LoginActivity;
+
+/**
+ * Created by emily on 15.06.2017.
+ */
+
+public class LoginActivity {
+
+}
---
+++ app/src/main/java/com/example/emily/triptalk/RegisterActivity/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.emily.triptalk.RegisterActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.emily.triptalk.R;
+
+/**
+ * Created by emily on 15.06.2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
